c32e11027ebb984f49a2764f1222169dba4add2c
xiaopies <xiaoshenma2016@gmail.com>
2020-11-19 14:24:14 -0500

    started RealNumber

---
+++ RealNumber.java
@@ -0,0 +1,14 @@
+public class RealNumber{
+  private double value;
+
+  public RealNumber(double v){
+    value = v;
+  }
+
+  public double getValue(){
+    return value;
+  }
+
+  public String toString(){
+    return ""+getValue();
+  }
