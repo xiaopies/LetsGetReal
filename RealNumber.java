9b466407db42b16b86717acc09eadf74237c3cc2
xiaopies <xiaoshenma2016@gmail.com>
2020-11-23 08:36:25 -0500

    add and multiply

--- RealNumber.java
+++ RealNumber.java
@@ -13,7 +13,20 @@ public class RealNumber{
     return ""+getValue();
   }
   public boolean equals(RealNumber other){
+    if (getValue()==0 || other.getValue()==0){
+      return (getValue()==0 && other.getValue()==0);
+    }
     double avg = (getValue()+ other.getValue())/2.0;
     return (Math.abs((this.getValue() - other.getValue())/avg) <= 0.00001);
   }
+  public RealNumber add(RealNumber other){
+     double a = getValue() + other.getValue();
+     RealNumber hi = new RealNumber(a);
+     return hi;
+  }
+  public RealNumber multiply(RealNumber other){
+     double a = getValue() * other.getValue();
+     RealNumber hi = new RealNumber(a);
+     return hi;
+   }
 }
--- Tester.java
+++ Tester.java
@@ -1,7 +1,9 @@
 public class Tester{
   public static void main(String[] args){
-    RealNumber a = new RealNumber(1.05);
-    RealNumber b = new RealNumber(1.0000);
+    RealNumber a = new RealNumber(0.25);
+    RealNumber b = new RealNumber(2);
     System.out.println(a.equals(b));
+    System.out.println(a.add(b));
+    System.out.println(a.multiply(b));
   }
 }
