536700c0cf7f1e6b384384fa8297892bbeb34e36
xiaopies <xiaoshenma2016@gmail.com>
2020-11-23 15:02:22 -0500

    fixed negatives in denominator

--- RationalNumber.java
+++ RationalNumber.java
@@ -3,7 +3,9 @@ public class RationalNumber extends RealNumber {
 
   public RationalNumber(int nume, int deno){
     super(0.0);//this value is ignored!
-    if (deno ==0){
+    numerator = nume;
+    denominator = deno;
+    if (deno==0){
       numerator = 0;
       denominator = 1;
     }
@@ -11,10 +13,7 @@ public class RationalNumber extends RealNumber {
       numerator = nume * -1;
       denominator = deno * -1;
     }
-    else{
-      numerator = nume;
-      denominator = deno;
-    }
+
   }
   public double getValue(){
     double n = numerator;
@@ -34,6 +33,7 @@ public class RationalNumber extends RealNumber {
     return (getNumerator() == other.getNumerator() && getDenominator() == other.getDenominator());
   }
   public String toString(){
+
     return numerator + "/" + denominator;
   }
   private static int gcd(int a, int b){
@@ -47,12 +47,46 @@ public class RationalNumber extends RealNumber {
     }
     return x;
   }
-  public void reduce(){
+  private void reduce(){
     int a = gcd(numerator, denominator);
     numerator = numerator/a;
     denominator = denominator/a;
-
+    if (getDenominator()==0){
+      numerator = 0;
+      denominator = 1;
+    }
+    if (getDenominator()<0){
+      numerator *= -1;
+      denominator *= -1;
+    }
   }
+  public RationalNumber multiply(RationalNumber other){
+    RationalNumber m = new RationalNumber(getNumerator()*other.getNumerator(), getDenominator() * other.getDenominator());
+    m.reduce();
+    return m;
+  }
+  public RationalNumber divide(RationalNumber other){
+    RationalNumber m = new RationalNumber(getNumerator()*other.getDenominator(), getDenominator() * other.getNumerator());
+    m.reduce();
+    return m;
+  }
+  public RationalNumber add(RationalNumber other){
+    int nome= getNumerator() * other.getDenominator();
+    int nwo= other.getNumerator() * getDenominator();
+    RationalNumber a= new RationalNumber( nome + nwo, other.getDenominator() * getDenominator());
+    a.reduce();
+    return a;
+  }
+  public RationalNumber subtract(RationalNumber other){
+    int nome= getNumerator() * other.getDenominator();
+    int nwo= other.getNumerator() * getDenominator();
+    int bottom = other.getDenominator() * getDenominator();
+    RationalNumber a= new RationalNumber( nome-nwo, bottom);
+    a.reduce();
+
+    return a;
+  }
+
 
 
 }
--- Tester.java
+++ Tester.java
@@ -8,7 +8,7 @@ public class Tester{
     System.out.println(a.divide(b));
     System.out.println(a.subtract(b));
     RationalNumber c = new RationalNumber(1, 2);
-    RationalNumber d = new RationalNumber(2, 1);
+    RationalNumber d = new RationalNumber(-2, 1);
     RationalNumber e = d.reciprocal();
     System.out.println(c.getValue());
     System.out.println(c.getNumerator());
@@ -18,8 +18,13 @@ public class Tester{
     System.out.println(c.toString());
     System.out.println(c);
     RationalNumber f = new RationalNumber(36, 72);
-    System.out.println(f);
-    f.reduce();
-    System.out.println(f);
+    System.out.println(c.multiply(d));
+    System.out.println(c.add(f));
+    RationalNumber g = new RationalNumber(1, 4);
+    System.out.println(g.subtract(f));
+    RationalNumber h = new RationalNumber(1, 0);
+    System.out.println(h);
+    RationalNumber i = new RationalNumber(1, -3);
+    System.out.println(i);
   }
 }
